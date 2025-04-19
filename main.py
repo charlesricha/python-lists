93cf9fff065676e00e444356718f350a2a5478f7
charlesricha <charlesmuthui206@gmail.com>
2025-04-19 16:10:04 +0300

    Learning py-lists

---
+++ main.py
@@ -0,0 +1,24 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)
+
+my_new_list = my_list.insert(1,15)
+
+print(my_list)
+
+
+my_list_two = []
+
+my_list_two.append([10,20,30,40])
+
+
+print(my_list_two)
+
+my_list_two[0].insert(1,15)
+
+print(my_list_two)

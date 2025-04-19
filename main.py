35d6e6275795ca4c1ebc60b80a6153f30c590fad
Charles Muthui <126247035+charlesricha@users.noreply.github.com>
2025-04-19 17:05:22 +0300

    main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+# Create an empty list called my_list
 my_list = []
 
+# Append elements: 10, 20, 30, 40
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
+print("After appending:", my_list) 
 
-print(my_list)
+# Insert 15 at the second position (index 1)
+my_list.insert(1, 15)
+print("After inserting 15:", my_list)  
 
-my_new_list = my_list.insert(1,15)
+# Extend with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+print("After extending:", my_list)
 
-print(my_list)
+# Remove the last element
+my_list.pop()
+print("After removing last element:", my_list)  
 
+# Sort in ascending order
+my_list.sort()
+print("After sorting:", my_list) 
 
-my_list_two = []
-
-my_list_two.append([10,20,30,40])
-
-
-print(my_list_two)
-
-my_list_two[0].insert(1,15)
-
-print(my_list_two)
+# Find and print the index of 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30) 
